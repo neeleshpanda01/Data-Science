c8e04c4cbba4b271b7befb02ea71478a954f9fa2
neeleshpanda01 <neelesh.kapil@gmail.com>
2025-10-15 09:20:56 +0530

    Create EX4.R

---
+++ EX4.R
@@ -0,0 +1,31 @@
+rnorm(5)
+
+rnorm(5,10,8)
+
+rnorm(n=5,sd=8,mean=100)
+
+rnorm(n=5,sd=8)
+
+seq(10,20,3)
+
+seq(from=10,to=20,by=3)
+
+seq(from=10,to=20,length.out=100)
+
+x <- c("a","b","c")
+A<-seq(from=10,to=20,along.with = x)
+
+rep(5,10)
+
+rep(5:6,10)
+
+rep(5:6,times = 10)
+
+rep(5:6,each = 10)
+
+rep(x,times = 5)
+
+rep(x,each = 5)
+
+B <-sqrt(A)
+B
